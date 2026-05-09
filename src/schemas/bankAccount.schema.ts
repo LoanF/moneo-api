4a960d9053519813bb69f88edfbc0ee461d58b45
Loan <loanfrancois@proton.me>
2026-05-09 14:08:26 +0200

    fix: limit account length

--- src/schemas/bankAccount.schema.ts
+++ src/schemas/bankAccount.schema.ts
@@ -13,14 +13,14 @@ export const BankAccountResponseSchema = z.object({
 
 export const CreateBankAccountSchema = z.object({
     id: z.uuid(),
-    name: z.string().min(1, "Nom du compte requis"),
+    name: z.string().min(1, "Nom du compte requis").max(30, "Le nom ne peut pas dépasser 30 caractères"),
     type: z.string().default('checking'),
     balance: z.number().default(0),
     currency: z.string().length(3).default('EUR')
 }).openapi('CreateAccountInput');
 
 export const UpdateBankAccountSchema = z.object({
-    name: z.string().min(1).optional(),
+    name: z.string().min(1).max(30, "Le nom ne peut pas dépasser 30 caractères").optional(),
     type: z.string().optional(),
     balance: z.number().optional(),
     currency: z.string().length(3).optional(),
